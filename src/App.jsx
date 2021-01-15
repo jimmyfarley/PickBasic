0cb192a21f937fa2bc987d857465915b48af0947
Jimmy <jimmyfarleydev@gmail.com>
2021-01-15 15:12:33 -0500

    include bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,26 @@
-import React, { Component } from 'react'
-import Navigation from './components/navigation';
-import Header from './components/header';
-import Features from './components/features';
-import About from './components/about';
-import Services from './components/services';
-import Gallery from './components/gallery';
-import Testimonials from './components/testimonials';
-import Team from './components/Team';
-import Contact from './components/contact';
-import JsonData from './data/data.json';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-export class App extends Component {
-  state = {
-    landingPageData: {},
-  }
-  getlandingPageData() {
-    this.setState({landingPageData : JsonData})
-  }
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './Home'
+import Video from './pages/video'
 
-  componentDidMount() {
-    this.getlandingPageData();
-  }
-
-  render() {
-    return (
-      <div>
-        <Navigation />
-        <Header data={this.state.landingPageData.Header} />
-        <Features data={this.state.landingPageData.Features} />
-        <About data={this.state.landingPageData.About} />
-        <Services data={this.state.landingPageData.Services} />
-        <Gallery />
-        <Testimonials data={this.state.landingPageData.Testimonials} />
-        <Team data={this.state.landingPageData.Team} />
-        <Contact data={this.state.landingPageData.Contact} />
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/demos/video'>
+          <Video />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
